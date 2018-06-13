96a9f710cba08e0ab6f7bac527924a022357c0b6
silvacarloss <ces.eduardo98@gmail.com>
2018-06-13 19:49:48 -0300

    Adiciona author

--- app/src/main/java/com/example/carlos/agenda/MainActivity.java
+++ app/src/main/java/com/example/carlos/agenda/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.carlos.agenda;
+/*
+* Author: Carlos Eduardo da Silva
+* Date: 06/13/2018
+*
+* */
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
